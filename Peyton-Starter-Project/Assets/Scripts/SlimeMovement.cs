8210ab4ee6f367406203b320fb6fa65d635bade5
Peyton Paul <peyton.z.paul@gmail.com>
2021-01-31 03:51:44 -0500

    Working Part 1

---
+++ Peyton-Starter-Project/Assets/Scripts/SlimeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 position = transform.position;
+        position.x = position.x + 0.07f * horizontal;
+        position.y = position.y + 0.07f * vertical;
+        transform.position = position;
+
+    }
+}
